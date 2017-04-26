e7711ced2aae5d26d0ca3182bb5d3ba1e159c052
kg <kylgrvs@gmail.com>
2017-04-26 15:04:01 -0700

    adds python template file

---
+++ edition4/py/template.py
@@ -0,0 +1,11 @@
+def func():
+    pass
+
+import unittest
+class TestHasAllUnique(unittest.TestCase):
+
+    def test_(self):
+        self.assertTrue(func())
+
+if __name__ == '__main__':
+    unittest.main()
